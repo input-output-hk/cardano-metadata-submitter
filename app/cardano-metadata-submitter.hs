918c1f8ee353b9f2f30cc638004422fddd82f042
Jimmy Hartzell <jah259@cornell.edu>
2021-01-07 22:01:27 +0000

    better argument parsing

--- app/cardano-metadata-submitter.hs
+++ app/cardano-metadata-submitter.hs
@@ -4,10 +4,28 @@ import Data.Aeson
 import Data.Aeson.Types
 import Prelude (String)
 import qualified Data.ByteString.Lazy as B
+import System.Console.ParseArgs
+
+data ArgumentIndex
+    = ArgumentIndexFilename
+    deriving (Show, Ord, Eq)
+
+argInfo :: [Arg ArgumentIndex]
+argInfo =
+    [ Arg ArgumentIndexFilename Nothing Nothing (argDataRequired "filename" ArgtypeString) "filename"
+    ]
 
 main :: IO ()
 main = do
-    (filename:_) <- getArgs
+    let argStrictness = ArgsParseControl ArgsComplete ArgsSoftDash
+
+    args <- parseArgsIO argStrictness argInfo
+    filename <- case getArgString args ArgumentIndexFilename of
+        Just fname -> return fname
+        Nothing -> do
+            hPutStrLn stderr ("No filename provided" :: String)
+            exitFailure
+
     fileContents <- B.readFile filename
     case decode fileContents of
         Just contents -> case parseEither parseRegistryEntry contents of
