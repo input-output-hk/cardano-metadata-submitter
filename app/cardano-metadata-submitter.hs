6b9c77368413ba982f31a52b27d5ea52d8c8ccf6
Jimmy Hartzell <jah259@cornell.edu>
2021-01-07 18:06:49 +0000

    absolutely basic parser-then-printer of registry

--- app/cardano-metadata-submitter.hs
+++ app/cardano-metadata-submitter.hs
@@ -1,4 +1,22 @@
 import Cardano.Prelude
+import Cardano.Metadata.GoguenRegistry
+import Data.Aeson
+import Data.Aeson.Types
+import Prelude (String)
+import qualified Data.ByteString.Lazy as B
 
 main :: IO ()
-main = return ()
+main = do
+    (filename:_) <- getArgs
+    fileContents <- B.readFile filename
+    case decode fileContents of
+        Just contents -> case parseEither parseRegistryEntry contents of
+            Right res -> do
+                print res
+                exitSuccess
+            Left err -> do
+                hPutStrLn stderr $ "Parse error: " <> err
+                exitFailure
+        Nothing -> do
+            hPutStrLn stderr ("JSON parse error" :: String)
+            exitFailure
