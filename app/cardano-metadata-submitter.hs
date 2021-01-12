d05e550418168288d5f0dcece714beb9b8d33f1e
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2021-01-12 13:18:29 -0500

    use ".draft" suffix for drafts, beginnings of processing key files

--- app/cardano-metadata-submitter.hs
+++ app/cardano-metadata-submitter.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-import Cardano.Prelude
+import Cardano.Binary
+import Cardano.Crypto.DSIGN
 import Cardano.Metadata.GoguenRegistry
 import Cardano.Metadata.Types
+import Cardano.Prelude
+
 import Control.Arrow (left)
 import Data.Aeson
 import Data.Aeson.Types
+import qualified Data.ByteString.Lazy as B
 import qualified Data.Text as T
 import qualified Options.Applicative as OA
-import Prelude (String, id) -- XXX Why NoImplicitPrelude??
-import qualified Data.ByteString.Lazy as B
+import Prelude (String, id)
+
+data DraftStatus = DraftStatusDraft | DraftStatusFinal
+  deriving Show
 
 data Arguments = Arguments
   { _ArgumentsFilename :: String
-  , _ArgumentsBackupFile :: Maybe String
+  , _ArgumentsSigningKeyFilename :: Maybe String
+  , _ArgumentsFinalize :: DraftStatus
   , _ArgumentsRegistryEntry :: GoguenRegistryEntry Maybe
   }
   deriving Show
@@ -32,7 +39,8 @@ withQuotes s = "\"" <> s <> "\"" -- XXX This is not at all the best way to do th
 argumentParser :: OA.Parser Arguments
 argumentParser = Arguments <$>
   OA.strArgument (OA.metavar "FILENAME") <*>
-  optional (OA.strOption (OA.long "backup" <> OA.short 'b' <> OA.metavar "BACKUP_FILE")) <*>
+  optional (OA.strOption (OA.long "keyfile" <> OA.short 'k' <> OA.metavar "KEY_FILE")) <*>
+  OA.flag DraftStatusDraft DraftStatusFinal (OA.long "finalize" <> OA.short 'f') <*>
     (GoguenRegistryEntry
       <$> optional (Subject <$> OA.strOption (OA.long "subject" <> OA.short 's' <> OA.metavar "SUBJECT"))
       <*> optional (poorlyAttest <$> wellKnownOption withQuotes (OA.long "name" <> OA.short 'n' <> OA.metavar "NAME"))
@@ -50,23 +58,25 @@ combineRegistryEntries new old = GoguenRegistryEntry
 
 main :: IO ()
 main = do
-    Arguments filename mBackup newEntryInfo <- OA.execParser $ OA.info (argumentParser <**> OA.helper) mempty
+  Arguments filename skFname finalize newEntryInfo <- OA.execParser $ OA.info (argumentParser <**> OA.helper) mempty
+  let draftFilename = filename <> ".draft"
 
-    fileContents <- B.readFile filename
+  registryJSONDraft <- eitherDecodeFileStrict draftFilename
+  registryJSON <- case registryJSONDraft of
+    Right res -> return res
+    Left err -> eitherDecodeFileStrict filename
 
-    WithOwnership owner record <- case decode fileContents of
-        Just contents -> case parseEither parseRegistryEntry contents of
-            Right res -> return res
-            Left err -> do
-                hPutStrLn stderr $ "Parse error: " <> err
-                exitFailure
-        Nothing -> do
-            hPutStrLn stderr ("JSON parse error" :: String)
-            exitFailure
-    let newRecordWithOwnership = WithOwnership owner $ combineRegistryEntries newEntryInfo record
+  WithOwnership owner record <- case registryJSON >>= parseEither parseRegistryEntry of
+    Right res -> return res
+    Left err -> do
+        hPutStrLn stderr $ "Parse error: " <> err
+        exitFailure
+  let newRecordWithOwnership = WithOwnership owner $ combineRegistryEntries newEntryInfo record
 
-    forM_ mBackup $ \backupFilename -> do
-      B.writeFile backupFilename fileContents
+  let outputFiles = case finalize of
+        DraftStatusFinal -> [draftFilename, filename]
+        DraftStatusDraft -> [draftFilename]
 
-    writeFile filename $ show (serializeRegistryEntry newRecordWithOwnership) <> "\n"
-    exitSuccess
+  forM_ outputFiles $ \fn ->
+    writeFile draftFilename $ show (serializeRegistryEntry newRecordWithOwnership) <> "\n"
+  exitSuccess
