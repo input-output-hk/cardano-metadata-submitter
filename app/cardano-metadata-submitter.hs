774e598df8297e498d4af1e71bbaea1abe5c557c
Jimmy Hartzell <jimmy.hartzell@obsidian.systems>
2021-01-14 13:53:20 -0500

    new conventions regarding draft files

--- app/cardano-metadata-submitter.hs
+++ app/cardano-metadata-submitter.hs
@@ -6,13 +6,13 @@ import Cardano.Metadata.Types
 import Cardano.Prelude
 
 import Control.Arrow (left)
-import Data.Aeson
-import Data.Aeson.Types
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Text as T
 import qualified Options.Applicative as OA
 import Prelude (String, id)
-import System.Directory
+import System.Directory (doesFileExist, renameFile)
 
 data DraftStatus = DraftStatusDraft | DraftStatusFinal
   deriving Show
@@ -22,11 +22,23 @@ data InputSource
   | InputSourceStdin
   deriving Show
 
+data EntryOperation
+  = EntryOperationInitialize
+  | EntryOperationRevise
+  deriving Show
+
 data FileInfo = FileInfo
-  { _FileInfoFilename :: String
+  { _FileInfoSubject :: String
+  , _FileInfoEntryOperation :: EntryOperation
   , _FileInfoDraftStatus :: DraftStatus
   }
   deriving Show
+
+canonicalFilename :: FileInfo -> String
+canonicalFilename fi = _FileInfoSubject fi <> ".json"
+
+draftFilename :: FileInfo -> String
+draftFilename fi = canonicalFilename fi <> ".draft"
 
 data Arguments = Arguments
   { _ArgumentsInputSource :: InputSource
@@ -41,7 +53,7 @@ wellKnownOption strTransform opts = OA.option wellKnownReader opts where
   wellKnownReader :: OA.ReadM (WellKnown p)
   wellKnownReader = OA.eitherReader $ \str -> do
     pv :: PropertyValue <- left T.unpack $ propertyValueFromString $ T.pack $ strTransform str
-    WellKnown pv <$> parseEither parseWellKnown pv
+    WellKnown pv <$> A.parseEither parseWellKnown pv
 
 poorlyAttest :: a -> Attested a
 poorlyAttest v = Attested [] v
@@ -61,12 +73,13 @@ argumentParser = Arguments <$>
 
     fileInfoArgumentParser :: OA.Parser FileInfo
     fileInfoArgumentParser = FileInfo <$>
-      OA.strArgument (OA.metavar "FILENAME") <*>
+      OA.strArgument (OA.metavar "SUBJECT") <*>
+      OA.flag EntryOperationInitialize EntryOperationRevise (OA.long "init" <> OA.short 'i') <*>
       OA.flag DraftStatusDraft DraftStatusFinal (OA.long "finalize" <> OA.short 'f')
 
     goguenRegistryEntryParser :: OA.Parser (PartialGoguenRegistryEntry)
     goguenRegistryEntryParser = GoguenRegistryEntry <$>
-      optional (Subject <$> OA.strOption (OA.long "subject" <> OA.short 's' <> OA.metavar "SUBJECT")) <*>
+      pure Nothing <*>
       optional (poorlyAttest <$> wellKnownOption withQuotes (OA.long "name" <> OA.short 'n' <> OA.metavar "NAME")) <*>
       optional (poorlyAttest <$> wellKnownOption withQuotes (OA.long "description" <> OA.short 'd' <> OA.metavar "DESCRIPTION")) <*>
       optional preimageParser
@@ -79,10 +92,13 @@ argumentParser = Arguments <$>
 combineRegistryEntries :: GoguenRegistryEntry Maybe -> GoguenRegistryEntry Maybe -> GoguenRegistryEntry Maybe
 combineRegistryEntries new old = GoguenRegistryEntry
   { _goguenRegistryEntry_subject = _goguenRegistryEntry_subject new <|> _goguenRegistryEntry_subject old
-  , _goguenRegistryEntry_name = _goguenRegistryEntry_name new <|> _goguenRegistryEntry_name old
-  , _goguenRegistryEntry_description = _goguenRegistryEntry_description new <|> _goguenRegistryEntry_description old
+  , _goguenRegistryEntry_name = _goguenRegistryEntry_name new `combineAttestedEntry` _goguenRegistryEntry_name old
+  , _goguenRegistryEntry_description = _goguenRegistryEntry_description new `combineAttestedEntry` _goguenRegistryEntry_description old
   , _goguenRegistryEntry_preimage = _goguenRegistryEntry_preimage new <|> _goguenRegistryEntry_preimage old
-  }
+  } where
+    combineAttestedEntry a b = case (a, b) of
+      -- (Just (Attested sigA valA), Just (Attested sigB valB)) | valA == valB -> Just $ Attested (sigA ++ sigB) valA
+      _ -> a <|> b
 
 attestField :: WellKnownProperty p => SignKeyDSIGN Ed25519DSIGN -> Subject -> Attested (WellKnown p) -> Attested (WellKnown p)
 attestField key subj (Attested att wk@(WellKnown raw structed)) = Attested attestations wk where
@@ -110,21 +126,23 @@ main = do
   attestKey <- mapM readKeyFile attestKeyFile
   ownerKey <- mapM readKeyFile ownerKeyFile
 
-  let draftFilename (FileInfo fn _) = fn <> ".draft"
-
-  registryJSON <- case inputInfo of
-    InputSourceFile fInfo -> do
-      let dfn = draftFilename fInfo
-      exists <- doesFileExist $ draftFilename fInfo
-      let readFn = if exists then dfn else _FileInfoFilename fInfo
-      eitherDecodeFileStrict readFn
+  WithOwnership oldOwner record <- case inputInfo of
     InputSourceStdin -> do
       input <- B.getContents
-      pure $ eitherDecode input
-
-  WithOwnership oldOwner record <- dieOnLeft "Parse error" $ do
-    json <- registryJSON
-    parseEither parseRegistryEntry json
+      parseJSON $ A.eitherDecode input
+    InputSourceFile fInfo@(FileInfo _ EntryOperationRevise _) -> do
+      let dfn = draftFilename fInfo
+      exists <- doesFileExist $ draftFilename fInfo
+      let readFn = if exists then dfn else canonicalFilename fInfo
+      json <- A.eitherDecodeFileStrict readFn
+      parseJSON json
+    InputSourceFile (FileInfo subj EntryOperationInitialize _) -> do
+      pure $ WithOwnership Nothing $ GoguenRegistryEntry
+        { _goguenRegistryEntry_subject = Just $ Subject $ T.pack subj
+        , _goguenRegistryEntry_name = Nothing
+        , _goguenRegistryEntry_description = Nothing
+        , _goguenRegistryEntry_preimage = Nothing
+        }
 
   let newRecord = combineRegistryEntries newEntryInfo record
 
@@ -143,7 +161,7 @@ main = do
     InputSourceFile fInfo -> do
       writeFile (draftFilename fInfo) outputString
       case _FileInfoDraftStatus fInfo of
-        DraftStatusFinal -> renameFile (draftFilename fInfo) $ _FileInfoFilename fInfo
+        DraftStatusFinal -> renameFile (draftFilename fInfo) $ canonicalFilename fInfo
         DraftStatusDraft -> pure ()
     InputSourceStdin -> do
       putStr outputString
@@ -159,3 +177,8 @@ main = do
     readKeyFile skFname = do
       lbs <- B.readFile skFname
       dieOnLeft "Error reading key file" $ left show $ decodeFullDecoder "Signing Key" decodeSignKeyDSIGN lbs
+
+    parseJSON :: Either String A.Value -> IO (WithOwnership Maybe PartialGoguenRegistryEntry)
+    parseJSON registryJSON = dieOnLeft "Parse error" $ do
+      json <- registryJSON
+      A.parseEither parseRegistryEntry json
